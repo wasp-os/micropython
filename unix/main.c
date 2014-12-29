10c9e2eb5c62bab4213d454ae6d66ba03f4ab872
Paul Sokolovsky <pfalcon@users.sourceforge.net>
2014-12-30 01:24:38 +0200

    unix: Support readline history saving to file, improves interactive usage.

--- unix/main.c
+++ unix/main.c
@@ -326,6 +326,8 @@ STATIC void set_sys_argv(char *argv[], int argc, int start_arg) {
 #endif
 
 int main(int argc, char **argv) {
+    prompt_read_history();
+
     mp_stack_set_limit(32768);
 
     pre_process_options(argc, argv);
@@ -508,6 +510,7 @@ int main(int argc, char **argv) {
 #endif
 
     //printf("total bytes = %d\n", m_get_total_bytes_allocated());
+    prompt_write_history();
     return ret & 0xff;
 }
 
